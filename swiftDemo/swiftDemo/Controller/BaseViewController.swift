19f57a3aa67ef1e36d5b5e2b445b5f98f797c460
xuhaoran <xuhaoran080@sunfit.cn>
2017-05-04 15:03:27 +0800

    整理文件目录

---
+++ swiftDemo/swiftDemo/Controller/BaseViewController.swift
@@ -0,0 +1,42 @@
+//
+//  BaseViewController.swift
+//  swiftDemo
+//
+//  Created by xuhaoran on 2017/5/4.
+//  Copyright © 2017年 SunFit. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    // MARK:- 生命周期
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK:- UI
+    
+    /// 设置导航条左键头返回
+    func setNaviBackBtn() {
+        
+        let btnItem = UIBarButtonItem.init(image: UIImage.init(named: "navigationBack"), style: .plain, target: self, action: #selector(popToLastVC))
+        btnItem.tintColor = UIColor.white
+        btnItem.imageInsets = UIEdgeInsetsMake(0, -10, 0, 10)
+        navigationItem.setLeftBarButton(btnItem, animated: true)
+        
+    }
+    
+    @objc func popToLastVC (){
+    
+        navigationController?.popViewController(animated: true)
+    }
+}
