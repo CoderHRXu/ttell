ef5c8082150189b032487d783705bb372c42613f
xuhaoran <xuhaoran080@sunfit.cn>
2017-07-07 11:20:26 +0800

    修复证书链接

--- swiftDemo/swiftDemo/Controller/HomePageViewController.swift
+++ swiftDemo/swiftDemo/Controller/HomePageViewController.swift
@@ -65,7 +65,7 @@ class HomePageViewController: BaseViewController ,UITableViewDelegate, UITableVi
     }
     @IBAction func cerBtnClick(_ sender: UIBarButtonItem) {
         
-        UIApplication.shared.openURL(URL(string: "https://172.16.88.230:8874/cer/myCA.cer")!)
+        UIApplication.shared.openURL(URL(string: "https://172.16.88.230:8874/cer/pubCer/selfSigned_pubCA.cer")!)
 
         
         
